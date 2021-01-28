f503543fe4e5a9a6cde8555ec934370ab2bd076f
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-27 23:32:36 -0600

    takes one input with getline

--- p1.c
+++ p1.c
@@ -3,22 +3,44 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+
 int main()
 {
-    char *args[3];
-    char line[10];
-    //size_t length = 0;
-    //ssize_t read;
-    args[1] = NULL;
-    args[0] = line;
+
+    char b[32];
+    char *buffer = b;
+    size_t len = 32;
+    ssize_t characters;
+    char *token;
+    const char space[2] = " ";
+    int i;
     printf("dash> ");
     while (1)
     {
-        scanf("%s", line);
+        characters = getline(&buffer, &len, stdin);
+        char *args[100];
+        token = strtok(buffer, space);
+        i = 0;
+        while (token != NULL)
+        {
+            args[i] = token;
+            /* printf("%s\n", args[i]); */
+            token = strtok(NULL, space);
+            //i++;
+        }
+
+        args[i + 1] = NULL;
+
         int id = fork();
         if (id == 0)
         {
-            execv(args[0], args);
+            /* printf("%d", i);
+            for (int j = 0; j < 5; j++)
+            {
+                printf("%s", args[j]);
+            } */
+            /* execv(buffer, args); */
+            execv(buffer, args);
         }
         else
         {
