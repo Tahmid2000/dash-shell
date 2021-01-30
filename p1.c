0a33309b87c8f42e0beab1da2ed0f49e40f768ef
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-29 19:46:29 -0600

    basic shell for interactive and batch done

--- p1.c
+++ p1.c
@@ -4,7 +4,21 @@
 #include <string.h>
 #include <fcntl.h>
 
-int main()
+void exitShell()
+{
+    exit(0);
+}
+
+void changeDirectory(char *dir)
+{
+    if (chdir(dir) != 0)
+    {
+        char error_message[30] = "An error has occurred w/ cd\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+    }
+}
+
+void shellBatch(char *file)
 {
     char *buffer;
     size_t len = 32;
@@ -13,9 +27,82 @@ int main()
     char *token;
     const char space[2] = " ";
     int i;
-    printf("dash> ");
+    FILE *fp;
+    fp = fopen(file, "r");
+    if (!fp)
+    {
+        char error_message[41] = "An error has occurred w opening the file\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+    }
+    characters = getline(&buffer, &len, fp);
+    while (characters >= 0)
+    {
+        if (buffer[0] != '\n')
+        {
+            char *args[100];
+            char *newline = strchr(buffer, '\n');
+            if (newline)
+                *newline = 0;
+            token = strtok(buffer, space);
+            args[i] = token;
+            i = 1;
+            while (token != NULL)
+            {
+                token = strtok(NULL, space);
+                args[i] = token;
+                i++;
+            }
+            if (strcmp(buffer, "exit") == 0)
+            {
+                free(buffer);
+                buffer = NULL;
+                exitShell();
+            }
+            else if (strcmp(buffer, "cd") == 0)
+            {
+                if (args[1] && !args[2])
+                    changeDirectory(args[1]);
+                else
+                {
+                    char error_message[35] = "An error has occurred w cd args\n";
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                }
+            }
+            else
+            {
+                int id = fork();
+                if (id == 0)
+                {
+                    if (execv(buffer, args) != 0)
+                    {
+                        char error_message[35] = "An error has occurred execv error\n";
+                        write(STDERR_FILENO, error_message, strlen(error_message));
+                    }
+                }
+                else
+                {
+                    wait(NULL);
+                }
+            }
+        }
+        characters = getline(&buffer, &len, fp);
+    }
+    fclose(fp);
+}
+
+void shellInteractive()
+{
+    char *buffer;
+    size_t len = 32;
+    buffer = (char *)malloc(len * sizeof(char));
+    ssize_t characters;
+    char *token;
+    const char space[2] = " ";
+    int i;
+
     while (1)
     {
+        printf("dash> ");
         characters = getline(&buffer, &len, stdin);
         char *args[100];
         char *newline = strchr(buffer, '\n');
@@ -30,19 +117,55 @@ int main()
             token = strtok(NULL, space);
             args[i] = token;
         }
-
-        //args[i + 1] = NULL;
-
-        int id = fork();
-        if (id == 0)
+        if (strcmp(buffer, "exit") == 0)
         {
-            execv(buffer, args);
+            free(buffer);
+            buffer = NULL;
+            exitShell();
+        }
+        else if (strcmp(buffer, "cd") == 0)
+        {
+            if (args[1] && !args[2])
+                changeDirectory(args[1]);
+            else
+            {
+                char error_message[35] = "An error has occurred w cd args\n";
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
         }
         else
         {
-            int id2 = wait(NULL);
-            printf("dash> ");
+            int id = fork();
+            if (id == 0)
+            {
+                if (execv(buffer, args) != 0)
+                {
+                    char error_message[30] = "An error has occurred execv\n";
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                }
+            }
+            else
+            {
+                wait(NULL);
+            }
         }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        shellBatch(argv[1]);
+    }
+    else if (argc > 2)
+    {
+        char error_message[30] = "An error has occurred\n";
+        write(STDERR_FILENO, error_message, strlen(error_message));
+    }
+    else
+    {
+        shellInteractive();
     }
     return 0;
 }
